4afc8be0d694629a272c70563630b5598d45fb8c
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-08-26 17:31:48 -0700

    combined reducers

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { createStore } from 'redux';
-import postReducer from './reducers/postReducer';
+import reducer from './reducers';
 
 export default createStore(
-  postReducer,
+  reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
