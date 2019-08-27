432a4d268412e26d15c23268897f521a261c7746
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-08-26 17:28:55 -0700

    comment reducer

--- src/reducers/commentReducer.js
+++ src/reducers/commentReducer.js
@@ -4,14 +4,27 @@ import {
 } from '../actions/commentActions';
 import { DELETE_POST } from '../actions/postActions';
 
-export const initialState = {
-  0: ['', '']
-};
+export const initialState = { };
 
 export default function commentReducer(state = initialState, action) {
   switch(action.type) {
-    case CREATE_COMMENT:
+    case CREATE_COMMENT: 
       return { ...state, 
-        [action.payload.postID]: [...(state[action.payload.postID] || []), action.payload.comment] };
+        [action.payload.postID]:
+          [...(state[action.payload.postID] || []), action.payload.comment] };
+    case DELETE_COMMENT:
+      return { ...state,
+        [action.payload.postID]:
+          [...state[action.payload.postID].slice(0, action.payload.commentID),
+            ...state[action.payload.postID].slice(action.payload.commentID + 1)] };
+    case DELETE_POST: {
+      const index = action.payload;
+      const newState = { ...state };
+      delete newState[index];
+      return newState;
+    }
+    default: return state;
   }
 }
+
+//you cant spread an undefined array, hence the or empty on ln 14
